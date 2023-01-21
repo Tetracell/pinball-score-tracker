29842fff4eaf50a3128e53663cb924e3071acadf
Tom LaTulipe <tlatulipe@gmail.com>
2023-01-21 01:51:07 -0500

    seperated into functions for readability

--- front-end/src/Components/AddScoreDrawer.jsx
+++ front-end/src/Components/AddScoreDrawer.jsx
@@ -16,12 +16,8 @@ const API = process.env.REACT_APP_API_URL;
 
 export const AddScoreDrawer = () => {
   const [visible, setVisible] = React.useState(false);
-  //For the GET request for players and tables
   const [players, setPlayers] = React.useState([{}]);
   const [tables, setTables] = React.useState([{}]);
-
-  //const [chosenPlayer, setChosenPlayer] = React.useState();
-  //const [chosenTable, setChosenTable] = React.useState();
 
   const [newScore, setNewScore] = React.useState({
     score: "",
@@ -50,8 +46,6 @@ export const AddScoreDrawer = () => {
     setVisible(false);
   };
 
-  //Something was / is making e.target.id come out as undefined, so I made these
-  // hardcoded functions with key names. Can't figure out the problem.
   const handleChangeScore = (e) => {
     setNewScore({ ...newScore, score: e.target.value });
   };
@@ -92,48 +86,18 @@ export const AddScoreDrawer = () => {
           textAlign="center"
           component="form"
           onSubmit={handleSubmit}
+          color="white"
+          backgroundColor="#151615"
         >
           <Typography variant="h5" sx={{ marginBottom: "5px" }}>
             Add New Score
           </Typography>
-          <InputLabel id="player-select-menu">Choose Player</InputLabel>
-          <Select
-            labelId="player-select-menu"
-            id="player"
-            onChange={handleChangePlayer}
-            value={newScore.player}
-          >
-            {players.map((player) => {
-              return (
-                <MenuItem value={player.playerid}>
-                  <strong>{player.initials}</strong> - {player.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
+          {choosePlayer()}
           <br />
-          <InputLabel id="table-select-menu">Choose Table</InputLabel>
-          <Select
-            labelId="table-select-menu"
-            id="machine"
-            onChange={handleChangeTable}
-            label="Table"
-            value={newScore.machine}
-          >
-            {tables.map((table) => {
-              return <MenuItem value={table.machineid}>{table.name}</MenuItem>;
-            })}
-          </Select>
+          {chooseTable()}
           <br />
           <br />
-          <InputLabel id="score-input-field">Enter Score</InputLabel>
-          <TextField
-            labelId="score-input-field"
-            id="score"
-            type="number"
-            onChange={handleChangeScore}
-            value={newScore.score}
-          ></TextField>
+          {enterScore()}
           <Button type="submit">Submit Score</Button>
           <Button onClick={handleClose}>Close Window</Button>
           <Button
@@ -147,4 +111,68 @@ export const AddScoreDrawer = () => {
       </Drawer>
     </>
   );
+
+  function choosePlayer() {
+    return (
+      <>
+        <InputLabel id="player-select-menu" sx={{ color: "lightblue" }}>
+          Choose Player
+        </InputLabel>
+        <Select
+          labelId="player-select-menu"
+          id="player"
+          sx={{ color: "lightblue" }}
+          onChange={handleChangePlayer}
+          value={newScore.player}
+        >
+          {players.map((player) => {
+            return (
+              <MenuItem value={player.playerid}>
+                <strong>{player.initials}</strong> - {player.name}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </>
+    );
+  }
+
+  function chooseTable() {
+    return (
+      <>
+        <InputLabel id="table-select-menu" sx={{ color: "lightblue" }}>
+          Choose Table
+        </InputLabel>
+        <Select
+          labelId="table-select-menu"
+          id="machine"
+          onChange={handleChangeTable}
+          label="Table"
+          value={newScore.machine}
+          sx={{ color: "lightblue" }}
+        >
+          {tables.map((table) => {
+            return <MenuItem value={table.machineid}>{table.name}</MenuItem>;
+          })}
+        </Select>
+      </>
+    );
+  }
+
+  function enterScore() {
+    return (
+      <>
+        <InputLabel id="score-input-field" sx={{ color: "lightblue" }}>
+          Enter Score
+        </InputLabel>
+        <TextField
+          labelId="score-input-field"
+          id="score"
+          type="number"
+          onChange={handleChangeScore}
+          value={newScore.score}
+        ></TextField>
+      </>
+    );
+  }
 };
