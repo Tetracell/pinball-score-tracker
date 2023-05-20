b686adee5628010a5e24c92b33950690b7514c1d
Tom LaTulipe <tlatulipe@gmail.com>
2023-05-20 14:05:21 -0400

    cors middleware test

--- back-end/app.js
+++ back-end/app.js
@@ -9,6 +9,25 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.50.239:3333');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // ROUTES
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Sanctum score database" });
