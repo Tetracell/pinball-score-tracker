8ac8375bc7da1ca33dcc2168831d60c6bda9851a
Tom LaTulipe <tlatulipe@gmail.com>
2023-06-07 20:10:45 -0400

    centered image of jim on homepage

--- front-end/src/Components/Home.jsx
+++ front-end/src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/Home.css'
 
 
 const dearLeader = require("../dear_leader.jpeg")
@@ -6,7 +7,7 @@ const dearLeader = require("../dear_leader.jpeg")
 export const Home = () => {
   return (
     <div>
-        <img src={dearLeader} alt="Dear Leader" height={600}></img>
+        <img src={dearLeader} alt="Dear Leader" id="jim"></img>
         <p/>To begin, please choose one of the options on the top of
         the Navigation bar, or if on mobile, press the hamburger
         menu in the top left.
